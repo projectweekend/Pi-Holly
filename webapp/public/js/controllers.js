d3692abca42ee1daa61b3a863a8483c7eeadbc56
Brian Hines <brian@projectweekend.net>
2014-01-08 22:04:08 -0600

    Put reporting service into controller

--- webapp/public/js/controllers.js
+++ webapp/public/js/controllers.js
@@ -22,30 +22,11 @@ cMod.controller( 'HomeCtrl', function ( $scope, socket ) {
 } );
 
 
-cMod.controller( 'SystemCtrl', function ( $scope, socket ) {
+cMod.controller( 'SystemCtrl', function ( $scope, socket, SystemTempReporting ) {
 
-    $scope.systemTempChart = {
-        options: {},
-        data: {
-            labels : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-            datasets : [
-                {
-                    fillColor : "rgba(151,187,205,0)",
-                    strokeColor : "#e67e22",
-                    pointColor : "rgba(151,187,205,0)",
-                    pointStrokeColor : "#e67e22",
-                    data : [4, 3, 5, 4, 6]
-                },
-                {
-                    fillColor : "rgba(151,187,205,0)",
-                    strokeColor : "#f1c40f",
-                    pointColor : "rgba(151,187,205,0)",
-                    pointStrokeColor : "#f1c40f",
-                    data : [8, 3, 2, 5, 4]
-                }
-            ],
-        }
-    };
+    $scope.systemTempReporting = SystemTempReporting;
+
+    $scope.systemTempReporting.buildRecentTempChart();
 
 } );
 
