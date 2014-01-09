aa556e1d2e3b2f26e86d4548ca2194097e2e4a23
Brian Hines <brian@projectweekend.net>
2014-01-08 20:43:04 -0600

    Put in some dummy data from tutorial because i wanna see this work

--- webapp/public/js/controllers.js
+++ webapp/public/js/controllers.js
@@ -14,6 +14,29 @@ cMod.controller( 'AppCtrl', function ( $scope, socket ) {
 
 
 cMod.controller( 'HomeCtrl', function ( $scope, socket ) {
+
+    $scope.systemTempChart = {
+        options: {},
+        data: {
+            labels : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+            datasets : [
+                {
+                    fillColor : "rgba(151,187,205,0)",
+                    strokeColor : "#e67e22",
+                    pointColor : "rgba(151,187,205,0)",
+                    pointStrokeColor : "#e67e22",
+                    data : [4, 3, 5, 4, 6]
+                },
+                {
+                    fillColor : "rgba(151,187,205,0)",
+                    strokeColor : "#f1c40f",
+                    pointColor : "rgba(151,187,205,0)",
+                    pointStrokeColor : "#f1c40f",
+                    data : [8, 3, 2, 5, 4]
+                }
+            ],
+        }
+    };
 	
 	socket.on( 'send:time', function ( data ) {
 		$scope.environmentalData = data;
