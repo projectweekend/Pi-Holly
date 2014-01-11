677f98d925091a435c8206dcefe4ff5e054c67ce
Brian Hines <brian@projectweekend.net>
2014-01-10 23:12:31 -0600

    Start work on calculating average temps

--- webapp/routes/api.js
+++ webapp/routes/api.js
@@ -2,11 +2,56 @@
  * Serve JSON to our AngularJS client
  */
 
- var appModels = require( '../models' );
+var appModels = require( '../models' );
+
 
 var errorHandler = function ( err, res ) {
     console.log( err );
     res.send( 500 );
+};
+
+
+var avgTempMapReduce = function ( units, output ) {
+    
+    var avgConfig = {};
+    avgConfig.out = { replace: "AverageSystemTemp" };
+
+    if ( units == 'F' ) {
+        
+        avgConfig.map =  function () {
+            emit( 1, this.fahrenheit );
+        };
+        
+        avgConfig.reduce =  function ( keyVal, fahrenheitValues ) {
+            return Array.avg( fahrenheitValues );
+        };
+        
+    } else {
+        
+        avgConfig.map =  function () {
+            emit( 1, this.celsius );
+        };
+        
+        avgConfig.reduce =  function ( keyVal, celsiusValues ) {
+            return Array.avg( celsiusValues );
+        };
+
+    }
+
+    SystemTemperatureData.mapReduce( avgConfig, function ( err, model, stats ) {
+        
+        model.find( {}, function ( err, data ) {
+            
+            if ( err ) {
+                return errorHandler( err, res);
+            }
+
+            output = data.value;
+
+        } );
+
+    } );
+
 };
 
 
@@ -126,5 +171,19 @@ exports.systemTemperatureDataReportingRecent = function ( req, res ) {
         return res.json( data );
 
     } );
+
+};
+
+exports.systemTemperatureDataReportingStats = function ( req, res ) {
+
+    var output = {
+        celsius: null,
+        fahrenheit: null
+    };
+
+    avgTempMapReduce( "F",  output.fahrenheit );
+    avgTempMapReduce( "C",  output.celsius );
+
+    return res.json( output );
 
 };
