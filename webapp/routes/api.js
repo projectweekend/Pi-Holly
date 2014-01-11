bc5a228be8e8ae41dc6a42954565baac488d8f7b
Brian Hines <brian@projectweekend.net>
2014-01-11 00:20:19 -0600

    Finalize map reduce calculations for temp stats API endpoint

--- webapp/routes/api.js
+++ webapp/routes/api.js
@@ -2,56 +2,13 @@
  * Serve JSON to our AngularJS client
  */
 
-var appModels = require( '../models' );
+var appModels = require( '../models' ),
+ async = require( 'async' );
 
 
 var errorHandler = function ( err, res ) {
     console.log( err );
     res.send( 500 );
-};
-
-
-var avgTempMapReduce = function ( units, output ) {
-    
-    var avgConfig = {};
-    avgConfig.out = { replace: "AverageSystemTemp" };
-
-    if ( units == 'F' ) {
-        
-        avgConfig.map =  function () {
-            emit( 1, this.fahrenheit );
-        };
-        
-        avgConfig.reduce =  function ( keyVal, fahrenheitValues ) {
-            return Array.avg( fahrenheitValues );
-        };
-        
-    } else {
-        
-        avgConfig.map =  function () {
-            emit( 1, this.celsius );
-        };
-        
-        avgConfig.reduce =  function ( keyVal, celsiusValues ) {
-            return Array.avg( celsiusValues );
-        };
-
-    }
-
-    SystemTemperatureData.mapReduce( avgConfig, function ( err, model, stats ) {
-        
-        model.find( {}, function ( err, data ) {
-            
-            if ( err ) {
-                return errorHandler( err, res);
-            }
-
-            output = data.value;
-
-        } );
-
-    } );
-
 };
 
 
@@ -158,6 +115,7 @@ exports.systemTemperatureDataReportingAll = function ( req, res ) {
 
 };
 
+
 exports.systemTemperatureDataReportingRecent = function ( req, res ) {
 
     var query = SystemTemperatureData.find().sort( '-date' ).limit(18);
@@ -174,16 +132,164 @@ exports.systemTemperatureDataReportingRecent = function ( req, res ) {
 
 };
 
+
 exports.systemTemperatureDataReportingStats = function ( req, res ) {
 
+    // The Temperature Stats output object
     var output = {
-        celsius: null,
-        fahrenheit: null
+        average: {
+            celsius: null,
+            fahrenheit: null
+        },
+        min: {
+            celsius: null,
+            fahrenheit: null
+        },
+        max: {
+            celsius: null,
+            fahrenheit: null
+        }
     };
 
-    avgTempMapReduce( "F",  output.fahrenheit );
-    avgTempMapReduce( "C",  output.celsius );
+    // performs fahrenheit average map reduce
+    var fahrenheitAverage = function ( callback ) {
 
-    return res.json( output );
+        var avgConfig = {};
+        avgConfig.out = { replace: "AverageSystemTempFahrenheit" };
+        
+        avgConfig.map =  function () {
+            emit( 1, this.fahrenheit );
+        };
+        
+        avgConfig.reduce =  function ( keyVal, fahrenheitValues ) {
+            return Array.avg( fahrenheitValues );
+        };
+        
+        SystemTemperatureData.mapReduce( avgConfig, function ( err, model, stats ) {
+            model.find( {}, function ( err, data ) {
+                if ( err ) {
+                    return callback( err );
+                }
+                output.average.fahrenheit = data[0].value;
+                callback();
+            } );
+        } );
+
+    };
+    
+    // performs celsius average map reduce
+    var celsiusAverage = function ( callback ) {
+
+        var avgConfig = {};
+        avgConfig.out = { replace: "AverageSystemTempCelsius" };
+
+        avgConfig.map =  function () {
+            emit( 1, this.celsius );
+        };
+        
+        avgConfig.reduce =  function ( keyVal, celsiusValues ) {
+            return Array.avg( celsiusValues );
+        };
+
+        SystemTemperatureData.mapReduce( avgConfig, function ( err, model, stats ) {
+            model.find( {}, function ( err, data ) {
+                if ( err ) {
+                    return callback( err );
+                }
+                output.average.celsius = data[0].value;
+                callback();
+            } );
+        } );
+
+    };
+
+    // performs fahrenheit min/max map reduce
+    var fahrenheitMinMax = function ( callback ) {
+        
+        var maxConfig = {};
+        maxConfig.out = { replace: "MinMaxSystemTempFahrenheit" };
+
+        maxConfig.map = function () {
+            var x = { fahrenheit: this.fahrenheit, _id: this._id };
+            emit( 1, { min: x, max: x } );
+        };
+
+        maxConfig.reduce = function (key, fahrenheitValues) {
+            var result = fahrenheitValues[0];
+            for ( var i = 1; i < fahrenheitValues.length; i++ ) {
+                if ( fahrenheitValues[i].min.fahrenheit < result.min.fahrenheit ) {
+                    result.min = fahrenheitValues[i].min;
+                }
+                if ( fahrenheitValues[i].max.fahrenheit > result.max.fahrenheit ) {
+                    result.max = fahrenheitValues[i].max;
+                }
+            }
+            return result;
+        };
+
+        SystemTemperatureData.mapReduce( maxConfig, function ( err, model, stats ) {
+            model.find( {}, function ( err, data ) {
+                if ( err ) {
+                    return callback( err );
+                }
+                output.min.fahrenheit = data[0].value.min.fahrenheit;
+                output.max.fahrenheit = data[0].value.max.fahrenheit;
+                callback();
+            } );
+        } );
+
+    };
+
+    // performs celsius min/max map reduce
+    var celsiusMinMax = function ( callback ) {
+        
+        var maxConfig = {};
+        maxConfig.out = { replace: "MinMaxSystemTempCelsius" };
+
+        maxConfig.map = function () {
+            var x = { celsius: this.celsius, _id: this._id };
+            emit( 1, { min: x, max: x } );
+        };
+
+        maxConfig.reduce = function (key, celsiusValues) {
+            var result = celsiusValues[0];
+            for ( var i = 1; i < celsiusValues.length; i++ ) {
+                if ( celsiusValues[i].min.celsius < result.min.celsius ) {
+                    result.min = celsiusValues[i].min;
+                }
+                if ( celsiusValues[i].max.celsius > result.max.celsius ) {
+                    result.max = celsiusValues[i].max;
+                }
+            }
+            return result;
+        };
+
+        SystemTemperatureData.mapReduce( maxConfig, function ( err, model, stats ) {
+            model.find( {}, function ( err, data ) {
+                if ( err ) {
+                    return callback( err );
+                }
+                output.min.celsius = data[0].value.min.celsius;
+                output.max.celsius = data[0].value.max.celsius;
+                callback();
+            } );
+        } );
+
+    };
+
+    // run all stat calculations async
+    async.parallel( [
+        fahrenheitAverage,
+        celsiusAverage,
+        fahrenheitMinMax,
+        celsiusMinMax
+    ],
+    // callback function for functions ran as async
+    function( err ){
+        if ( err ) {
+            return next( errorHandler( err, res ) );
+        }
+        return res.json( output );
+    } );
 
 };
