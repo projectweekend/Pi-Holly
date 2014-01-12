9585c89bb1caf614265ab20b76eca4d63252176b
Brian Hines <brian@projectweekend.net>
2014-01-12 17:28:28 -0600

    Make parser return memory data as int

--- workers/utils/parsers.py
+++ workers/utils/parsers.py
@@ -3,7 +3,14 @@ def parse_temp_value(system_result):
 
 
 def parse_memory_values(system_result):
+    
     result_lines = system_result.splitlines()
+    
     header_list = result_lines[0].strip().split()
-    data_list = result_lines[1].replace("Mem:", "").strip().split()
+    
+    data_line = result_lines[1]    
+    for i in ["Mem:", "M", "B"]:
+        data_line = data_line.replace(i, "")
+    data_list = [int(x) for x in data_line.strip().split()]
+
     return zip(header_list, data_list)
