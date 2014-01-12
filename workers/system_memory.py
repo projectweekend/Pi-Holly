115a7dcaa1472686c551acea2dcf1564e1b80ce0
Brian Hines <brian@projectweekend.net>
2014-01-12 17:07:35 -0600

    Add system_memory worker process

---
+++ workers/system_memory.py
@@ -0,0 +1,26 @@
+import subprocess
+
+import utils
+
+
+SHELL_COMMAND = ["/usr/bin/free", "-h"]
+POST_URL = "http://127.0.0.1/api/system-memory-data"
+
+
+def get_system_memory():
+    system_result = subprocess.check_output(SHELL_COMMAND)
+    system_memory = utils.parse_memory_values(system_result)
+    return dict(system_memory)
+
+
+def worker():
+    memory_data = get_system_memory()
+    post_status = utils.make_json_post(POST_URL, memory_data)
+    if post_status != 201:
+        # TODO: Add some logging when POST fails
+        pass
+    return
+
+
+if __name__ == "__main__":
+    worker()
