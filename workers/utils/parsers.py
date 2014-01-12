234c2d205ee7a60de4f121c926c275dba4d1a1a2
Brian Hines <brian@projectweekend.net>
2014-01-12 17:01:43 -0600

    Add util to parse system memory data from raw result string

--- workers/utils/parsers.py
+++ workers/utils/parsers.py
@@ -1,2 +1,9 @@
 def parse_temp_value(system_result):
     return float(system_result.split("=")[1].split("'")[0])
+
+
+def parse_memory_values(system_result):
+    result_lines = system_result.splitlines()
+    header_list = result_lines[0].strip().split()
+    data_list = result_lines[1].replace("Mem:", "").strip().split()
+    return (header_list, data_list)
