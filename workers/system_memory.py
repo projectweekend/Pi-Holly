c0900371bd5ca2ba3667de59b66bfb10c4929705
Brian Hines <brian@projectweekend.net>
2014-01-12 18:50:45 -0600

    Make worker actually post data

--- workers/system_memory.py
+++ workers/system_memory.py
@@ -15,12 +15,11 @@ def get_system_memory():
 
 def worker():
     memory_data = get_system_memory()
-    print(memory_data)
-    # post_status = utils.make_json_post(POST_URL, memory_data)
-    # if post_status != 201:
-    #     # TODO: Add some logging when POST fails
-    #     pass
-    # return
+    post_status = utils.make_json_post(POST_URL, memory_data)
+    if post_status != 201:
+        # TODO: Add some logging when POST fails
+        pass
+    return
 
 
 if __name__ == "__main__":
