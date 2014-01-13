94804462317bec12d30ea9600d6348c39a659dc1
Brian Hines <brian@projectweekend.net>
2014-01-12 20:08:32 -0600

    Finish system storage worker

--- workers/system_storage.py
+++ workers/system_storage.py
@@ -5,3 +5,22 @@ import utils
 
 SHELL_COMMAND = ["/bin/df", "-BM"]
 POST_URL = "http://127.0.0.1/api/system-storage-data"
+
+
+def get_system_storage():
+    system_result = subprocess.check_output(SHELL_COMMAND)
+    system_storage = utils.parse_storage_values(system_result)
+    return dict(system_storage)
+
+
+def worker():
+    storage_data = get_system_storage()
+    post_status = utils.make_json_post(POST_URL, storage_data)
+    if post_status != 201:
+        # TODO: Add some logging when POST fails
+        pass
+    return
+
+
+if __name__ == "__main__":
+    worker()        
