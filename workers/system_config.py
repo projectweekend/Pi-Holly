b46c798349e4e0324a875a9c99606bfe36f74892
Brian Hines <brian@projectweekend.net>
2014-01-15 21:38:14 -0600

    Add worker to capture system config data

---
+++ workers/system_config.py
@@ -0,0 +1,26 @@
+import subprocess
+
+import utils
+
+
+SHELL_COMMAND = ["/opt/vc/bin/vcgencmd", "get_config", "int"]
+POST_URL = "http://127.0.0.1/api/system-config-data"
+
+
+def get_system_config():
+    system_result = subprocess.check_output(SHELL_COMMAND)
+    system_config = utils.parse_config_values(system_result)
+    return dict(system_config)
+
+
+def worker():
+    config_data = get_system_config()
+    post_status = utils.make_json_post(POST_URL, config_data)
+    if post_status != 201:
+        # TODO: Add some logging when POST fails
+        pass
+    return    
+
+
+if __name__ == "__main__":
+    worker()
