a045fd495e36f72c9ff6baa4283c295f0c8e8f76
Brian Hines <brian@projectweekend.net>
2014-01-19 15:01:02 -0600

    Update database helpers

--- workers/utils/database_helpers.py
+++ workers/utils/database_helpers.py
@@ -14,3 +14,14 @@ def get_collection(collection_name):
 def get_documents_for_collection(collection_name):
     collection = get_collection(collection_name)
     return [d for d in collection.find()]
+
+
+def populate_articles_collection(articles_list):
+    articles_collection = get_collection('newsarticles')
+    for a in articles_list:
+        articles_collection.insert({
+            'title': a.title,
+            'summary': a.summary,
+            'url': a.url,
+            'keywords': a.keywords
+        })
