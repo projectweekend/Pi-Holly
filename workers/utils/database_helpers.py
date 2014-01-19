c0411199a20f9fc178a12924bfd57506c2d3f08e
Brian Hines <brian@projectweekend.net>
2014-01-19 15:05:40 -0600

    Add helper to empty collection before populating for the day

--- workers/utils/database_helpers.py
+++ workers/utils/database_helpers.py
@@ -16,6 +16,11 @@ def get_documents_for_collection(collection_name):
     return [d for d in collection.find()]
 
 
+def empty_articles_collection():
+    articles_collection = get_collection('newsarticles')
+    articles_collection.remove()
+
+
 def populate_articles_collection(articles_list):
     articles_collection = get_collection('newsarticles')
     for a in articles_list:
