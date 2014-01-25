59127ef126ce72def41c2a484b39dffe73ff1576
Brian Hines <brian@projectweekend.net>
2014-01-25 09:21:45 -0600

    Small change

--- workers/utils/twitter_helpers.py
+++ workers/utils/twitter_helpers.py
@@ -8,8 +8,7 @@ TWITTER_ACCESS_TOKEN_KEY = os.getenv('TWITTER_ACCESS_TOKEN_KEY', '')
 TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET', '')
 
 def get_twitter_client():
-    client = twitter.Api(consumer_key=TWITTER_CONSUMER_KEY,
+    return twitter.Api(consumer_key=TWITTER_CONSUMER_KEY,
                             consumer_secret=TWITTER_CONSUMER_SECRET,
                             access_token_key=TWITTER_ACCESS_TOKEN_KEY,
                             access_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
-    return client
