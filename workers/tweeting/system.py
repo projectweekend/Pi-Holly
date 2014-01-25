de654ab55482606852446b809f02c649f1c20624
Brian Hines <brian@projectweekend.net>
2014-01-25 10:01:53 -0600

    Add module to facilitate automatic tweeting

---
+++ workers/tweeting/system.py
@@ -0,0 +1,30 @@
+import utils
+
+
+class CPUTemperatureTweeter(object):
+
+    temperature_data = {}
+    status_message = ""
+    
+    def __init__(self):
+        self._get_temperature_data()
+        self._build_status_message()
+        self.twitter_client = self._connect_to_twitter()
+        super(CPUTemperatureTweeter, self).__init__()
+
+    def _connect_to_twitter(self):
+        return utils.get_twitter_client()
+
+    def _get_temperature_data(self):
+        self.temperature_data = utils.get_latest_system_temperature()
+
+    def _build_status_message(self):
+        if self.temperature_data:
+            celsius = self.temperature_data['celsius']
+            fahrenheit = self.temperature_data['fahrenheit']
+            message_template = "My current CPU temperature is: {0} F/{1} C"
+            self.status_message = message_template.format(fahrenheit, celsius)
+
+    def tweet(self):
+        if self.status_message:
+            self.twitter_client.PostUpdate(self.status_message)
