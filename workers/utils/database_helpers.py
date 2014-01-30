5e9528d7b3e7446ded88f3d3d3f0f544a0b86b0b
Brian Hines <brian@projectweekend.net>
2014-01-29 18:57:12 -0600

    Add database helpers to get temp and humidity data

--- workers/utils/database_helpers.py
+++ workers/utils/database_helpers.py
@@ -19,3 +19,13 @@ def get_documents_for_collection(collection_name):
 def get_latest_system_temperature():
     collection = get_collection('systemtemperaturedatas')
     return collection.find_one(sort=[('date', -1)])
+
+
+def get_latest_indoor_temperature():
+    collection = get_collection('indoortemperaturedatas')
+    return collection.find_one(sort=[('date', -1)])
+
+
+def get_latest_indoor_humidity():
+    collection = get_collection('indoorhumiditydatas')
+    return collection.find_one(sort=[('date', -1)])
