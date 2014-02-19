fed5af117c0935af14f788abbff661fdb4bd4e8c
Brian Hines <brian@projectweekend.net>
2014-02-19 07:40:32 -0600

    Start re-working async calculations in the indoor stat routes

--- webapp/routes/indoorTemperatureStatsAPI.js
+++ webapp/routes/indoorTemperatureStatsAPI.js
@@ -8,7 +8,107 @@ var errorHandler = function ( err, res ) {
 };
 
 
+var buildFahrenheitAverageCallback = function ( config ) {
+
+    // performs fahrenheit average map reduce
+    return function ( callback ) {
+
+        var avgConfig = {
+            out: { replace: config.collection },
+            map: function () { emit( 1, this.fahrenheit ); },
+            reduce: function ( keyVal, fahrenheitValues ) { return Array.avg( fahrenheitValues ); }
+        };
+
+        IndoorTemperatureData.mapReduce( avgConfig, function ( err, model, stats ) {
+            if ( err ) {
+                console.log( err );
+                return callback( err );
+            }
+            model.find( config.query, function ( err, data ) {
+                if ( err ) {
+                    return callback( err );
+                }
+                output.average.fahrenheit = data[0].value;
+                callback();
+            } );
+        } );
+
+    };
+
+};
+
+var buildCelsiusAverageCallback = function ( config ) {
+    
+    // performs celsius average map reduce
+    return function ( callback ) {
+
+        var avgConfig = {
+            out: { replace: config.collection },
+            map: function () { emit( 1, this.celsius ); },
+            reduce: function ( keyVal, celsiusValues ) { return Array.avg( celsiusValues ); }
+        };
+
+        IndoorTemperatureData.mapReduce( avgConfig, function ( err, model, stats ) {
+            if ( err ) {
+                console.log( err );
+                return callback( err );
+            }
+            model.find( config.query, function ( err, data ) {
+                if ( err ) {
+                    return callback( err );
+                }
+                output.average.celsius = data[0].value;
+                callback();
+            } );
+        } );
+
+    };
+};
+
+
 exports.indoorTemperatureStatsOverall = function ( req, res ) {
+
+    var output = {
+        average: {
+            celsius: 0,
+            fahrenheit: 0
+        },
+        min: {
+            celsius: 0,
+            fahrenheit: 0
+        },
+        max: {
+            celsius: 0,
+            fahrenheit: 0
+        }
+    };
+
+    var fahrenheitAverage = buildFahrenheitAverageCallback(
+        {
+            collection: "AverageIndoorOverallTempFahrenheit",
+            query: {}
+        }
+    );
+
+    var celsiusAverage = buildCelsiusAverageCallback(
+        {
+            collection: "AverageIndoorOverallTempCelsius",
+            query: {}
+        }
+    );
+
+    // run all stat calculations async
+    async.parallel( [
+        fahrenheitAverage,
+        celsiusAverage
+    ],
+    // callback function for processes running async
+    function( err ){
+        if ( err ) {
+            return errorHandler( err, res );
+        }
+        return res.json( output );
+    } );
 
 };
 
