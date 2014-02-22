3f393db484639413399b4ffbb6d0ddc32d2c4c6a
Brian Hines <brian@projectweekend.net>
2014-02-22 13:31:43 -0600

    Add stats humidity routes for humidity data

--- webapp/routes/indoorHumidityStatsAPI.js
+++ webapp/routes/indoorHumidityStatsAPI.js
@@ -12,6 +12,7 @@ var errorHandler = function ( err, res ) {
 exports.indoorHumidityStatsOverall = function ( req, res ) {
     
     var output = {
+        label: "Overall",
         average: {
             percent: 0
         },
@@ -59,14 +60,182 @@ exports.indoorHumidityStatsOverall = function ( req, res ) {
 
 exports.indoorHumidityStatsDay = function ( req, res ) {
 
+    var output = {
+        label: "Today",
+        average: {
+            percent: 0
+        },
+        min: {
+            percent: 0
+        },
+        max: {
+            percent: 0
+        }
+    };
+
+    var d = new Date();
+    var currentDate = d.getDate();
+    var currentMonth = d.getMonth();
+    var currentYear = d.getFullYear();
+
+    var q = {
+        "date": {
+            "$gte": new Date( currentYear, currentMonth, currentDate ),
+            "$lt": new Date( currentYear, currentMonth, currentDate + 1 )
+        }
+    };
+
+    var getHumidityAverage = asyncCallbackHelpers.buildHumidityAverageCallback (
+        {
+            model: IndoorHumidityData,
+            collection: "AverageIndoorDayHumidity",
+            query: q
+        },
+        output
+    );
+
+    var getHumidityMinMax = asyncCallbackHelpers.buildHumidityMinMaxCallback (
+        {
+            model: IndoorHumidityData,
+            collection: "MinMaxIndoorDayHumidity",
+            query: q
+        },
+        output
+    );
+
+    // run all stat calculations async
+    async.parallel( [
+        getHumidityAverage,
+        getHumidityMinMax
+    ],
+    // callback function for processes running async
+    function( err ){
+        if ( err ) {
+            return errorHandler( err, res );
+        }
+        return res.json( output );
+    } );
+
 };
 
 
 exports.indoorHumidityStatsWeek = function ( req, res ) {
 
+    var output = {
+        label: "Last 7 Days",
+        average: {
+            percent: 0
+        },
+        min: {
+            percent: 0
+        },
+        max: {
+            percent: 0
+        }
+    };
+
+    var d = new Date();
+    var currentDate = d.getDate();
+    var currentMonth = d.getMonth();
+    var currentYear = d.getFullYear();
+
+    var q = {
+        "date": {
+            "$gte": new Date( currentYear, currentMonth, currentDate - 7 ),
+            "$lt": new Date( currentYear, currentMonth, currentDate + 1 )
+        }
+    };
+
+    var getHumidityAverage = asyncCallbackHelpers.buildHumidityAverageCallback (
+        {
+            model: IndoorHumidityData,
+            collection: "AverageIndoorWeekHumidity",
+            query: q
+        },
+        output
+    );
+
+    var getHumidityMinMax = asyncCallbackHelpers.buildHumidityMinMaxCallback (
+        {
+            model: IndoorHumidityData,
+            collection: "MinMaxIndoorWeekHumidity",
+            query: q
+        },
+        output
+    );
+
+    // run all stat calculations async
+    async.parallel( [
+        getHumidityAverage,
+        getHumidityMinMax
+    ],
+    // callback function for processes running async
+    function( err ){
+        if ( err ) {
+            return errorHandler( err, res );
+        }
+        return res.json( output );
+    } );
+
 };
 
 
 exports.indoorHumidityStatsMonth = function ( req, res ) {
+
+    var output = {
+        label: "Last 30 Days",
+        average: {
+            percent: 0
+        },
+        min: {
+            percent: 0
+        },
+        max: {
+            percent: 0
+        }
+    };
+
+    var d = new Date();
+    var currentDate = d.getDate();
+    var currentMonth = d.getMonth();
+    var currentYear = d.getFullYear();
+
+    var q = {
+        "date": {
+            "$gte": new Date( currentYear, currentMonth, currentDate - 30 ),
+            "$lt": new Date( currentYear, currentMonth, currentDate + 1 )
+        }
+    };
+
+    var getHumidityAverage = asyncCallbackHelpers.buildHumidityAverageCallback (
+        {
+            model: IndoorHumidityData,
+            collection: "AverageIndoorMonthHumidity",
+            query: q
+        },
+        output
+    );
+
+    var getHumidityMinMax = asyncCallbackHelpers.buildHumidityMinMaxCallback (
+        {
+            model: IndoorHumidityData,
+            collection: "MinMaxIndoorMonthHumidity",
+            query: q
+        },
+        output
+    );
+
+    // run all stat calculations async
+    async.parallel( [
+        getHumidityAverage,
+        getHumidityMinMax
+    ],
+    // callback function for processes running async
+    function( err ){
+        if ( err ) {
+            return errorHandler( err, res );
+        }
+        return res.json( output );
+    } );
 
 };
