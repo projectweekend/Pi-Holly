0a8331d8e31c603ca3886ea1768375df071f25c5
Brian Hines <brian@projectweekend.net>
2014-02-23 10:33:26 -0600

    Build new API routes

--- webapp/routes/starbugTemperatureStatsAPI.js
+++ webapp/routes/starbugTemperatureStatsAPI.js
@@ -1,5 +1,6 @@
 var appModels = require( '../models' ),
- async = require( 'async' );
+	asyncCallbackHelpers = require( '../helpers/AsyncCallbacks' ),
+	async = require( 'async' );
 
 
 var errorHandler = function ( err, res ) {
@@ -8,8 +9,8 @@ var errorHandler = function ( err, res ) {
 };
 
 
-exports.starbugTemperatureDataStats = function ( req, res ) {
-    
+exports.starbugTemperatureStatsOverall = function ( req, res ) {
+
     // The Temperature Stats output object
     var output = {
         average: {
@@ -26,162 +27,300 @@ exports.starbugTemperatureDataStats = function ( req, res ) {
         }
     };
 
-    // performs fahrenheit average map reduce
-    var fahrenheitAverage = function ( callback ) {
+    var getFahrenheitAverage = asyncCallbackHelpers.buildFahrenheitAverageCallback (
+        {
+            model: StarbugTemperatureData,
+            collection: "AverageStarbugTempFahrenheit",
+            query: {}
+        },
+        output
+    );
 
-        var avgConfig = {};
-        avgConfig.out = { replace: "AverageStarbugTempFahrenheit" };
-        
-        avgConfig.map =  function () {
-            emit( 1, this.fahrenheit );
-        };
-        
-        avgConfig.reduce =  function ( keyVal, fahrenheitValues ) {
-            return Array.avg( fahrenheitValues );
-        };
-        
-        StarbugTemperatureData.mapReduce( avgConfig, function ( err, model, stats ) {
-            
-            if ( err ) {
-                console.log( err );
-                return callback( err );
-            }
+    var getCelsiusAverage = asyncCallbackHelpers.buildCelsiusAverageCallback (
+        {
+            model: StarbugTemperatureData,
+            collection: "AverageStarbugTempCelsius",
+            query: {}
+        },
+        output
+    );
 
-            model.find( {}, function ( err, data ) {
-                if ( err ) {
-                    return callback( err );
-                }
-                output.average.fahrenheit = data[0].value;
-                callback();
-            } );
-        } );
+    var getFahrenheitMinMax = asyncCallbackHelpers.buildFahrenheitMinMaxCallback(
+        {
+            model: StarbugTemperatureData,
+            collection: "MinMaxStarbugTempFahrenheit",
+            query: {}
+        },
+        output
+    );
 
-    };
-
-    // performs celsius average map reduce
-    var celsiusAverage = function ( callback ) {
-
-        var avgConfig = {};
-        avgConfig.out = { replace: "AverageStarbugTempCelsius" };
-
-        avgConfig.map =  function () {
-            emit( 1, this.celsius );
-        };
-        
-        avgConfig.reduce =  function ( keyVal, celsiusValues ) {
-            return Array.avg( celsiusValues );
-        };
-
-        StarbugTemperatureData.mapReduce( avgConfig, function ( err, model, stats ) {
-
-            if ( err ) {
-                console.log( err );
-                return callback( err );
-            }
-
-            model.find( {}, function ( err, data ) {
-                if ( err ) {
-                    return callback( err );
-                }
-                output.average.celsius = data[0].value;
-                callback();
-            } );
-        } );
-
-    };
-
-    // performs fahrenheit min/max map reduce
-    var fahrenheitMinMax = function ( callback ) {
-        
-        var maxConfig = {};
-        maxConfig.out = { replace: "MinMaxStarbugTempFahrenheit" };
-
-        maxConfig.map = function () {
-            var x = { fahrenheit: this.fahrenheit, _id: this._id };
-            emit( 1, { min: x, max: x } );
-        };
-
-        maxConfig.reduce = function (key, fahrenheitValues) {
-            var result = fahrenheitValues[0];
-            for ( var i = 1; i < fahrenheitValues.length; i++ ) {
-                if ( fahrenheitValues[i].min.fahrenheit < result.min.fahrenheit ) {
-                    result.min = fahrenheitValues[i].min;
-                }
-                if ( fahrenheitValues[i].max.fahrenheit > result.max.fahrenheit ) {
-                    result.max = fahrenheitValues[i].max;
-                }
-            }
-            return result;
-        };
-
-        StarbugTemperatureData.mapReduce( maxConfig, function ( err, model, stats ) {
-
-            if ( err ) {
-                console.log( err );
-                return callback( err );
-            }
-
-            model.find( {}, function ( err, data ) {
-                if ( err ) {
-                    return callback( err );
-                }
-                output.min.fahrenheit = data[0].value.min.fahrenheit;
-                output.max.fahrenheit = data[0].value.max.fahrenheit;
-                callback();
-            } );
-        } );
-
-    };
-
-    // performs celsius min/max map reduce
-    var celsiusMinMax = function ( callback ) {
-        
-        var maxConfig = {};
-        maxConfig.out = { replace: "MinMaxStarbugTempCelsius" };
-
-        maxConfig.map = function () {
-            var x = { celsius: this.celsius, _id: this._id };
-            emit( 1, { min: x, max: x } );
-        };
-
-        maxConfig.reduce = function (key, celsiusValues) {
-            var result = celsiusValues[0];
-            for ( var i = 1; i < celsiusValues.length; i++ ) {
-                if ( celsiusValues[i].min.celsius < result.min.celsius ) {
-                    result.min = celsiusValues[i].min;
-                }
-                if ( celsiusValues[i].max.celsius > result.max.celsius ) {
-                    result.max = celsiusValues[i].max;
-                }
-            }
-            return result;
-        };
-
-        StarbugTemperatureData.mapReduce( maxConfig, function ( err, model, stats ) {
-
-            if ( err ) {
-                console.log( err );
-                return callback( err );
-            }
-
-            model.find( {}, function ( err, data ) {
-                if ( err ) {
-                    return callback( err );
-                }
-                output.min.celsius = data[0].value.min.celsius;
-                output.max.celsius = data[0].value.max.celsius;
-                callback();
-            } );
-        } );
-
-    };
+    var getCelsiusMinMax = asyncCallbackHelpers.buildCelsiusMinMaxCallback(
+        {
+            model: StarbugTemperatureData,
+            collection: "MinMaxStarbugTempCelsius",
+            query: {}
+        },
+        output
+    );
 
     // run all stat calculations async
     async.parallel( [
-        fahrenheitAverage,
-        celsiusAverage,
-        fahrenheitMinMax,
-        celsiusMinMax
+        getFahrenheitAverage,
+        getCelsiusAverage,
+        getFahrenheitMinMax,
+        getCelsiusMinMax
+    ],
+    // callback function for processes running async
+    function( err ){
+        if ( err ) {
+            return errorHandler( err, res );
+        }
+        return res.json( output );
+    } );
+
+};
+
+
+exports.starbugTemperatureStatsDay = function ( req, res ) {
+
+    // The Temperature Stats output object
+    var output = {
+        average: {
+            celsius: null,
+            fahrenheit: null
+        },
+        min: {
+            celsius: null,
+            fahrenheit: null
+        },
+        max: {
+            celsius: null,
+            fahrenheit: null
+        }
+    };
+
+    var d = new Date();
+    var currentDate = d.getDate();
+    var currentMonth = d.getMonth();
+    var currentYear = d.getFullYear();
+
+    var q = {
+        "date": {
+            "$gte": new Date( currentYear, currentMonth, currentDate ),
+            "$lt": new Date( currentYear, currentMonth, currentDate + 1 )
+        }
+    };
+
+    var getFahrenheitAverage = asyncCallbackHelpers.buildFahrenheitAverageCallback (
+        {
+            model: StarbugTemperatureData,
+            collection: "AverageStarbugTempDayFahrenheit",
+            query: q
+        },
+        output
+    );
+
+    var getCelsiusAverage = asyncCallbackHelpers.buildCelsiusAverageCallback (
+        {
+            model: StarbugTemperatureData,
+            collection: "AverageStarbugTempDayCelsius",
+            query: q
+        },
+        output
+    );
+
+    var getFahrenheitMinMax = asyncCallbackHelpers.buildFahrenheitMinMaxCallback(
+        {
+            model: StarbugTemperatureData,
+            collection: "MinMaxStarbugTempDayFahrenheit",
+            query: q
+        },
+        output
+    );
+
+    var getCelsiusMinMax = asyncCallbackHelpers.buildCelsiusMinMaxCallback(
+        {
+            model: StarbugTemperatureData,
+            collection: "MinMaxStarbugTempDayCelsius",
+            query: q
+        },
+        output
+    );
+
+    // run all stat calculations async
+    async.parallel( [
+        getFahrenheitAverage,
+        getCelsiusAverage,
+        getFahrenheitMinMax,
+        getCelsiusMinMax
+    ],
+    // callback function for processes running async
+    function( err ){
+        if ( err ) {
+            return errorHandler( err, res );
+        }
+        return res.json( output );
+    } );
+
+};
+
+
+exports.starbugTemperatureStatsWeek = function ( req, res ) {
+
+    // The Temperature Stats output object
+    var output = {
+        average: {
+            celsius: null,
+            fahrenheit: null
+        },
+        min: {
+            celsius: null,
+            fahrenheit: null
+        },
+        max: {
+            celsius: null,
+            fahrenheit: null
+        }
+    };
+
+    var d = new Date();
+    var currentDate = d.getDate();
+    var currentMonth = d.getMonth();
+    var currentYear = d.getFullYear();
+
+    var q = {
+        "date": {
+            "$gte": new Date( currentYear, currentMonth, currentDate - 7 ),
+            "$lt": new Date( currentYear, currentMonth, currentDate + 1 )
+        }
+    };
+
+    var getFahrenheitAverage = asyncCallbackHelpers.buildFahrenheitAverageCallback (
+        {
+            model: StarbugTemperatureData,
+            collection: "AverageStarbugTempWeekFahrenheit",
+            query: q
+        },
+        output
+    );
+
+    var getCelsiusAverage = asyncCallbackHelpers.buildCelsiusAverageCallback (
+        {
+            model: StarbugTemperatureData,
+            collection: "AverageStarbugTempWeekCelsius",
+            query: q
+        },
+        output
+    );
+
+    var getFahrenheitMinMax = asyncCallbackHelpers.buildFahrenheitMinMaxCallback(
+        {
+            model: StarbugTemperatureData,
+            collection: "MinMaxStarbugTempWeekFahrenheit",
+            query: q
+        },
+        output
+    );
+
+    var getCelsiusMinMax = asyncCallbackHelpers.buildCelsiusMinMaxCallback(
+        {
+            model: StarbugTemperatureData,
+            collection: "MinMaxStarbugTempWeekCelsius",
+            query: q
+        },
+        output
+    );
+
+    // run all stat calculations async
+    async.parallel( [
+        getFahrenheitAverage,
+        getCelsiusAverage,
+        getFahrenheitMinMax,
+        getCelsiusMinMax
+    ],
+    // callback function for processes running async
+    function( err ){
+        if ( err ) {
+            return errorHandler( err, res );
+        }
+        return res.json( output );
+    } );
+
+};
+
+
+exports.starbugTemperatureStatsMonth = function ( req, res ) {
+
+    // The Temperature Stats output object
+    var output = {
+        average: {
+            celsius: null,
+            fahrenheit: null
+        },
+        min: {
+            celsius: null,
+            fahrenheit: null
+        },
+        max: {
+            celsius: null,
+            fahrenheit: null
+        }
+    };
+
+    var d = new Date();
+    var currentDate = d.getDate();
+    var currentMonth = d.getMonth();
+    var currentYear = d.getFullYear();
+
+    var q = {
+        "date": {
+            "$gte": new Date( currentYear, currentMonth, currentDate - 30 ),
+            "$lt": new Date( currentYear, currentMonth, currentDate + 1 )
+        }
+    };
+
+    var getFahrenheitAverage = asyncCallbackHelpers.buildFahrenheitAverageCallback (
+        {
+            model: StarbugTemperatureData,
+            collection: "AverageStarbugTempMonthFahrenheit",
+            query: q
+        },
+        output
+    );
+
+    var getCelsiusAverage = asyncCallbackHelpers.buildCelsiusAverageCallback (
+        {
+            model: StarbugTemperatureData,
+            collection: "AverageStarbugTempMonthCelsius",
+            query: q
+        },
+        output
+    );
+
+    var getFahrenheitMinMax = asyncCallbackHelpers.buildFahrenheitMinMaxCallback(
+        {
+            model: StarbugTemperatureData,
+            collection: "MinMaxStarbugTempMonthFahrenheit",
+            query: q
+        },
+        output
+    );
+
+    var getCelsiusMinMax = asyncCallbackHelpers.buildCelsiusMinMaxCallback(
+        {
+            model: StarbugTemperatureData,
+            collection: "MinMaxStarbugTempMonthCelsius",
+            query: q
+        },
+        output
+    );
+
+    // run all stat calculations async
+    async.parallel( [
+        getFahrenheitAverage,
+        getCelsiusAverage,
+        getFahrenheitMinMax,
+        getCelsiusMinMax
     ],
     // callback function for processes running async
     function( err ){
