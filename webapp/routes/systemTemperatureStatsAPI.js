d199a9bf191a5179fb55564d7ed7720b5209ad61
Brian Hines <brian@projectweekend.net>
2014-02-23 01:19:59 -0600

    Refactor to use new async callback builders

--- webapp/routes/systemTemperatureStatsAPI.js
+++ webapp/routes/systemTemperatureStatsAPI.js
@@ -28,162 +28,48 @@ exports.systemTemperatureStatsOverall = function ( req, res ) {
         }
     };
 
-    // performs fahrenheit average map reduce
-    var fahrenheitAverage = function ( callback ) {
+    var getFahrenheitAverage = asyncCallbackHelpers.buildFahrenheitAverageCallback (
+        {
+            model: SystemTemperatureData,
+            collection: "AverageSystemTempFahrenheit",
+            query: {}
+        },
+        output
+    );
 
-        var avgConfig = {};
-        avgConfig.out = { replace: "AverageSystemTempFahrenheit" };
-        
-        avgConfig.map =  function () {
-            emit( 1, this.fahrenheit );
-        };
-        
-        avgConfig.reduce =  function ( keyVal, fahrenheitValues ) {
-            return Array.avg( fahrenheitValues );
-        };
-        
-        SystemTemperatureData.mapReduce( avgConfig, function ( err, model, stats ) {
-            
-            if ( err ) {
-                console.log( err );
-                return callback( err );
-            }
+    var getCelsiusAverage = asyncCallbackHelpers.buildCelsiusAverageCallback (
+        {
+            model: SystemTemperatureData,
+            collection: "AverageSystemTempCelsius",
+            query: {}
+        },
+        output
+    );
 
-            model.find( {}, function ( err, data ) {
-                if ( err ) {
-                    return callback( err );
-                }
-                output.average.fahrenheit = data[0].value;
-                callback();
-            } );
-        } );
+    var getFahrenheitMinMax = asyncCallbackHelpers.buildFahrenheitMinMaxCallback(
+        {
+            model: SystemTemperatureData,
+            collection: "MinMaxSystemTempFahrenheit",
+            query: {}
+        },
+        output
+    );
 
-    };
-    
-    // performs celsius average map reduce
-    var celsiusAverage = function ( callback ) {
-
-        var avgConfig = {};
-        avgConfig.out = { replace: "AverageSystemTempCelsius" };
-
-        avgConfig.map =  function () {
-            emit( 1, this.celsius );
-        };
-        
-        avgConfig.reduce =  function ( keyVal, celsiusValues ) {
-            return Array.avg( celsiusValues );
-        };
-
-        SystemTemperatureData.mapReduce( avgConfig, function ( err, model, stats ) {
-
-            if ( err ) {
-                console.log( err );
-                return callback( err );
-            }
-
-            model.find( {}, function ( err, data ) {
-                if ( err ) {
-                    return callback( err );
-                }
-                output.average.celsius = data[0].value;
-                callback();
-            } );
-        } );
-
-    };
-
-    // performs fahrenheit min/max map reduce
-    var fahrenheitMinMax = function ( callback ) {
-        
-        var maxConfig = {};
-        maxConfig.out = { replace: "MinMaxSystemTempFahrenheit" };
-
-        maxConfig.map = function () {
-            var x = { fahrenheit: this.fahrenheit, _id: this._id };
-            emit( 1, { min: x, max: x } );
-        };
-
-        maxConfig.reduce = function (key, fahrenheitValues) {
-            var result = fahrenheitValues[0];
-            for ( var i = 1; i < fahrenheitValues.length; i++ ) {
-                if ( fahrenheitValues[i].min.fahrenheit < result.min.fahrenheit ) {
-                    result.min = fahrenheitValues[i].min;
-                }
-                if ( fahrenheitValues[i].max.fahrenheit > result.max.fahrenheit ) {
-                    result.max = fahrenheitValues[i].max;
-                }
-            }
-            return result;
-        };
-
-        SystemTemperatureData.mapReduce( maxConfig, function ( err, model, stats ) {
-
-            if ( err ) {
-                console.log( err );
-                return callback( err );
-            }
-
-            model.find( {}, function ( err, data ) {
-                if ( err ) {
-                    return callback( err );
-                }
-                output.min.fahrenheit = data[0].value.min.fahrenheit;
-                output.max.fahrenheit = data[0].value.max.fahrenheit;
-                callback();
-            } );
-        } );
-
-    };
-
-    // performs celsius min/max map reduce
-    var celsiusMinMax = function ( callback ) {
-        
-        var maxConfig = {};
-        maxConfig.out = { replace: "MinMaxSystemTempCelsius" };
-
-        maxConfig.map = function () {
-            var x = { celsius: this.celsius, _id: this._id };
-            emit( 1, { min: x, max: x } );
-        };
-
-        maxConfig.reduce = function (key, celsiusValues) {
-            var result = celsiusValues[0];
-            for ( var i = 1; i < celsiusValues.length; i++ ) {
-                if ( celsiusValues[i].min.celsius < result.min.celsius ) {
-                    result.min = celsiusValues[i].min;
-                }
-                if ( celsiusValues[i].max.celsius > result.max.celsius ) {
-                    result.max = celsiusValues[i].max;
-                }
-            }
-            return result;
-        };
-
-        SystemTemperatureData.mapReduce( maxConfig, function ( err, model, stats ) {
-
-            if ( err ) {
-                console.log( err );
-                return callback( err );
-            }
-
-            model.find( {}, function ( err, data ) {
-                if ( err ) {
-                    return callback( err );
-                }
-                output.min.celsius = data[0].value.min.celsius;
-                output.max.celsius = data[0].value.max.celsius;
-                callback();
-            } );
-        } );
-
-    };
+    var getCelsiusMinMax = asyncCallbackHelpers.buildCelsiusMinMaxCallback(
+        {
+            model: SystemTemperatureData,
+            collection: "MinMaxSystemTempCelsius",
+            query: {}
+        },
+        output
+    );
 
     // run all stat calculations async
     async.parallel( [
-        fahrenheitAverage,
-        celsiusAverage,
-        fahrenheitMinMax,
-        celsiusMinMax
+        getFahrenheitAverage,
+        getCelsiusAverage,
+        getFahrenheitMinMax,
+        getCelsiusMinMax
     ],
     // callback function for processes running async
     function( err ){
